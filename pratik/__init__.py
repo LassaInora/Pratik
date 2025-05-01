f00a3bd913343a68da10bddb1492fe8f0325f46d
LassaInora <lassa.inora@gmail.com>
2025-05-01 16:47:32 +0200

    Commit 1.3.2
    Add:
     -
    
    Update:
     - Bug in call pratik
    
    Deprecated:
     - get_path - Removed for 1.5.x
    
    Remove:
     -

--- pratik/__init__.py
+++ pratik/__init__.py
@@ -1,8 +1,14 @@
 import json
 import pathlib
 
-with open(pathlib.Path(__file__).parent.parent.parent / 'metadata.json') as f:
+path = pathlib.Path(__file__).parent / 'metadata.json'
+if path.exists():
+    __DEBUG__ = False
+else:
+    path = pathlib.Path(__file__).parent.parent / 'metadata.json'
+    __DEBUG__ = True
+with open(path) as f:
     _METADATA = json.load(f)
-    __author__ = _METADATA['author']
+    __author__ = _METADATA['authors']
     __version__ = _METADATA['version']
     __description__ = _METADATA['description']
